03aa20f3f0a0351ac943250928e77b54c60ce1a9
MohammadAmin <fahimi13823182@gmail.com>
2022-06-04 10:19:01 +0430

    Course.cpp is done

---
+++ Course.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include "Course.h"
+using namespace std;
+Course::Course(string str,int u,double* m):name(str),unit(u),mark(m){
+    mark=new double;
+}
+    Course::Course(const Course& r){
+        name = r.name;
+        unit = r.unit;
+        mark = new double;
+        *mark = *(r.mark);
+    }
+    Course::~Course(){delete mark;}
+    string Course::getName(){return this->name;}
+    int Course::getUnit(){return this->unit;}
+    double* Course::getMark(){return this->mark;}
+    void Course::setName(string str){name=str;}
+    void Course::setUnit(int u){unit=u;}
+    void Course::setMark(double* m){*mark=*m;}
+    ostream& operator<<(ostream& out,const Course& r){
+        out<<"Name:"<<r.name<<endl;
+        out<<"Unit:"<<r.unit<<endl;
+        out<<"Mark:"<<*(r.mark)<<endl;
+        return out;
+    }
+    istream& operator>>(istream& in,Course& r){
+        cout<<"Name:";
+        in>>r.name;
+        cout<<endl;
+        cout<<"Unit:";
+        in>>r.unit;
+        cout<<endl;
+        cout<<"Mark:";
+        in>>*(r.mark);
+        cout<<endl;
+    }
