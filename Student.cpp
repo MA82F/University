e5a3485d5c58c202b4fe9b3f2e4c62d5a59d00bf
MohammadAmin <fahimi13823182@gmail.com>
2022-06-04 17:08:54 +0430

    validate id done & used in constructor

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,16 @@
 #include <cmath>
 #include "Student.h"
 using namespace std;
-Student::Student(std::string str1,std::string str2,std::string str3,double* w,
-            Course* c,std::string str4,int n):Person(str1,str2,str3,w){
-                courses = new Course("none",0,new double);
-                *courses = *c;
-                fieldOfStudy = str4;
-                numOfCourses = n;
+Student::Student(string str1,string str2,string str3,double* w,Course* c,
+                 string str4,int n):Person(str1,str2,str3,w){
+    if (validate(str3)==0){
+        cout<<"invalid id";
+        exit(0);
+    }
+    courses = new Course("none",0,new double);
+    *courses = *c;
+    fieldOfStudy = str4;
+    numOfCourses = n;
 }
 Student::Student(const Student& r):Person(*this){
     fieldOfStudy = r.fieldOfStudy;
@@ -52,4 +56,37 @@ istream& operator>>(std::istream& in,Student& r){
     }
     cout<<endl;
     return in;
+}
+bool validate(string str){
+    if (!(str.length()>=8 && str.length()<=10))
+        return 0;
+    if (!(stoi(str.substr(0,1))>84 && stoi(str.substr(0,1))<=99)) //between 84 to 00
+        return 0;
+    if (str[2]!='*')
+        return 0;
+    if (str.length()==10){
+        for (int i = 3; i <= 4; i++)
+            if (isdigit(str[i]))
+                return 0;
+        for (int i = 5; i <= str.length(); i++)
+            if (!(str[i]=='0' || str[i]=='1' || str[i]=='2' || str[i]=='3' ||
+                str[i]=='7' || str[i]=='8' || str[i]=='9'))
+                return 0;
+    }
+    if (str.length()==9){
+        if (isdigit(str[3]))
+            return 0;
+        for (int i = 4; i <= str.length(); i++)
+            if (!(str[i]=='0' || str[i]=='1' || str[i]=='2' || str[i]=='3' ||
+                str[i]=='7' || str[i]=='8' || str[i]=='9'))
+                return 0;
+        
+    }
+    if (str.length()==8){
+        for (int i = 3; i <= str.length(); i++)
+            if (!(str[i]=='0' || str[i]=='1' || str[i]=='2' || str[i]=='3' ||
+                str[i]=='7' || str[i]=='8' || str[i]=='9'))
+                return 0;
+    }
+    return 1;  
 }
